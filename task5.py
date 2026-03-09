7bd0dadee4d7bb600cb503e12eed60c96e864aee
Eli Leutert <Elijah.Leutert594@students.cmcss.net>
2026-03-09 08:50:00 -0500

    dsfsf

---
+++ task5.py
@@ -0,0 +1,38 @@
+good = r"""
+
+ ,--.     ,--.
+(  O )   (  O )
+ `--'  \  `--'
+        \   _
+  >-.   /   /| 
+     `-.__.'
+"""
+
+
+
+bad = r"""
+
+
+      _..._
+    ,'     `.
+  ,'         `.
+,'    _   ,-.  `.
+|    (_)  `-'   |
+|        >      |
+|     ,----.    |
+|    /,-""-.\   |
+`.  |/      "  ,'
+  `.         ,'
+    `._____,'
+"""
+escaped = True
+
+if escaped:
+    outcome = "Legend: I escaped yes."
+    print(good)
+else:
+    outcome = "Doom: im stuck here"
+    print(bad)
+
+
+print(outcome)
